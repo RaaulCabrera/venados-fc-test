08f9b92df4c734d7bb777cbcb7354182744d58a0
raaul- <raaul-@live.com.mx>
2018-07-31 23:21:50 -0500

    Add coordinator layout in home fragment

--- app/src/main/java/com/raulcabrera/venadostest/activities/VenadosActivity.java
+++ app/src/main/java/com/raulcabrera/venadostest/activities/VenadosActivity.java
@@ -28,6 +28,7 @@ public class VenadosActivity extends AppCompatActivity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_venados);
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		toolbar.setTitle("");
 		setSupportActionBar(toolbar);
 
 		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -49,28 +50,6 @@ public class VenadosActivity extends AppCompatActivity
 		} else {
 			super.onBackPressed();
 		}
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.venados, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
-
-		return super.onOptionsItemSelected(item);
 	}
 
 	@SuppressWarnings("StatementWithEmptyBody")
