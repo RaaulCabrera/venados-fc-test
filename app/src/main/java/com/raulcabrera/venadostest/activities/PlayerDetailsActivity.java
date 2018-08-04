fd0bc20917b3192beb371571d44058f49f8dbc58
raaul- <raaul-@live.com.mx>
2018-08-03 22:27:47 -0500

    Prototype of the application

---
+++ app/src/main/java/com/raulcabrera/venadostest/activities/PlayerDetailsActivity.java
@@ -0,0 +1,23 @@
+package com.raulcabrera.venadostest.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Window;
+
+import com.raulcabrera.venadostest.R;
+
+public class PlayerDetailsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_player_details);
+	}
+
+}
